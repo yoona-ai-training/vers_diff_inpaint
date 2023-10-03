a3489612f7691ae2945e7d4964fd458ddcd6efb8
yoona-ai-training <140383574+yoona-ai-training@users.noreply.github.com>
2023-10-03 21:04:12 +0200

    Update download.py

--- download.py
+++ download.py
@@ -6,8 +6,6 @@ def download_model():
   pipe = VersatileDiffusionDualGuidedPipeline.from_pretrained(
     "shi-labs/versatile-diffusion"
   ).to("cuda:0")
-  
-  pipe.remove_unused_weights()
 
 if __name__ == "__main__":
     download_model()
